78df4e2002893dba8fcd1976eee008a266f937c7
Vishnu071 <vishnu91594@gmail.com>
2025-06-12 02:07:08 +0530

    Add root URL view for homepage

--- backend/zentask/urls.py
+++ backend/zentask/urls.py
@@ -1,9 +1,13 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("ZenTask API is running!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/tasks/', include('tasks.urls')),
+    path('', home),  # This will serve a message at the root URL
 ]
